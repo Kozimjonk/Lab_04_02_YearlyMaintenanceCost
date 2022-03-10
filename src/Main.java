d1ee5487a0cd61633a7402a7ad9d894571dac090
Kozimjon <kozimkuchkarov77@gmail.com>
2022-03-10 17:45:31 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+                double winterCost = 366.99;
+                double springCost = 100.00;
+                double summerCost = 110.99;
+                double fallCost = 230.00;
+
+                double yearlyCost = winterCost + springCost + summerCost + fallCost;
+                System.out.println("Your home maintenance cost for Winter: $" + winterCost + " Spring: $" + springCost + " Summer: $" + summerCost + " Fall: $" + fallCost + " Total: $" + yearlyCost);
+
+
+    }
+}
